5571c926aa030c63a36f202aa49e04ca10b86627
byronboots <bootsbyron@gmail.com>
2022-12-05 22:02:57 -0800

    Solution part 2 code and comments

--- Day3AdventOfCode2022.py
+++ Day3AdventOfCode2022.py
@@ -53,17 +53,68 @@ for value in letter_list:
 for value in letter_list:
     priority_total += priorities[value]
 
-for compartment in rucksacks:
-    elf_1 = ''
-    elf_2 = ''
-    elf_3 = ''
+# Part 2
+# Creates new lists for each group
+group_1 = []
+group_2 = []
+group_3 = []
+new_letter_list = []
+new_value_list = []
+group_bag_1 = ''
+group_bag_2 = ''
+group_bag_3 = ''
+new_priority_total = 0
+# Takes every 3rd rucksack starting at the first rucksack and adds to group 1 list
+for compartment in rucksacks[0::3]:
+    # group_bag_1 = compartment
+    group_1.append(compartment)
+# Takes every 3rd rucksack starting at the second rucksack and adds to group 2 list
+for compartment in rucksacks[1::3]:
+    # group_bag_2 = compartment
+    group_2.append(compartment)
+# Takes every 3rd rucksack starting at the third rucksack and adds to group 3 list
+for compartment in rucksacks[2::3]:
+    # group_bag_3 = compartment
+    group_3.append(compartment)
+
+# letter_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+# Sets a counter index to 0
+index = 0
+# While loop that runs as long as the index is less than the length of the remaining rucksacks list to iterate through
+while index < len(rucksacks):
+    # Sets 3 variables equal to be the first, second, and third strings up next in the rucksacks list each time the loop runs
+    group_bag_1 = rucksacks[index]
+    group_bag_2 = rucksacks[index+1]
+    group_bag_3 = rucksacks[index+2]
+    # iterates through the letters in the first bag from the group of rucksacks to see if any letters appear in both of
+    # the other two bags in the group. Once found the letter is added to a new list, the index is increased by 3 and
+    # the loop is broken so we stop searching through letters for a match
+    for letter in group_bag_1:
+        if letter in group_bag_2 and letter in group_bag_3:
+            new_letter_list.append(letter)
+            index += 3
+            break
+
+# Creates a new list with the assigned priority values
+for value in new_letter_list:
+    new_value_list.append(priorities[value])
+# Sums the total priority values across all rucksacks
+for value in new_letter_list:
+    new_priority_total += priorities[value]
 
 
 
-
-print(new_list)
-print(letter_list)
-print(value_list)
-print(priority_total)
+# print(new_list)
+# print(letter_list)
+# print(value_list)
+# print(priority_total)
+print(group_1)
+print(group_2)
+print(group_3)
+print(new_letter_list)
+print(new_value_list)
+print(new_priority_total)
 
 
