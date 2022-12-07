069b6cf3d3cd43c7bb0a1084a1407a6c6f25bc03
byronboots <bootsbyron@gmail.com>
2022-12-06 19:33:05 -0800

    Solution to part 1

---
+++ Day4AdventOfCode2022.py
@@ -0,0 +1,51 @@
+
+# Input data into a list where each elf pair is a single index in the list
+with open('day4.in') as file:
+    pairs = [elf for elf in file.read().strip().split("\n")]
+
+print(pairs)
+
+# Split created list into a list where each elf assigned section is a single index in the list
+new_pairs = []
+for elf in pairs:
+    elf_1 = elf.split(',')[0]
+    elf_2 = elf.split(',')[1]
+    new_pairs.append(elf_1)
+    new_pairs.append(elf_2)
+
+print(new_pairs)
+
+# new_pairs = ['37-87', '36-87', '3-98', '3-84']
+
+
+# Sets a counter index to 0
+index = 0
+# Sets a counter index to 0 for all pairs that have one elf which fully contains another elf section
+count_fully_contain = 0
+# A loop to go through and take each elf starting number and ending number and put them into their own start and end
+# variable
+while index < len(new_pairs):
+    # Sets two variable equal to every other index in the list
+    elf_one = new_pairs[index]
+    elf_two = new_pairs[index+1]
+    # Takes two variables and splits into 4 using the - as the delimiter. Converts to integer type
+    elf_one_start = int(elf_one.split('-')[0])
+    elf_one_end = int(elf_one.split('-')[1])
+    elf_two_start = int(elf_two.split('-')[0])
+    elf_two_end = int(elf_two.split('-')[1])
+    # If statement to see if the first elf contains the second elf section Adds one to counter if so
+    if elf_one_start <= elf_two_start and elf_one_end >= elf_two_end:
+        count_fully_contain += 1
+    # If statement to see if the second elf contains the first elf section. Adds one to counter if so
+    elif elf_two_start <= elf_one_start and elf_two_end >= elf_one_end:
+        count_fully_contain += 1
+    # Adds two to counter index to get to the next pair of elves the next time through the loop
+    index += 2
+
+print(elf_one_start)
+print(elf_one_end)
+print(elf_two_start)
+print(elf_two_end)
+
+print(count_fully_contain)
+
